5cbe0842f5331f5c55f4dc169da147adb576092c
Rayon Hunte <rayon.hunte@gmail.com>
2024-10-30 14:25:14 -0400

    stable build added buttons

--- app/button/page.tsx
+++ app/button/page.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import Button  from "@/components/ui/Button"; // Ensure the casing matches the actual file name
-
-export function ButtonDemo() {
-  return <Button>Button</Button>;
-}
+import Button from "@/components/ui/Button"; // Ensure the casing matches the actual file name
 
 const ButtonPage: React.FC = () => {
   return (
-    <div>
+    <div className="flex justify-center items-center h-screen space-x-4">
       <Button variant="outline">Cancel</Button>
-      <Button>Deploy</Button>
+      <Button variant="destructive">Deploy</Button>
+      <Button variant="secondary">Save</Button>
+      <Button variant="default">Edit</Button>
+      <Button variant="link">Learn more</Button>
+      <Button variant="ghost">Delete</Button>
     </div>
   );
 };
